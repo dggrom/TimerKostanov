7bcda2e91f9c618a2958906e7569a7cfe80adce7
dgggrom-NB <dg-grom@mail.ru>
2019-02-17 17:55:49 +0200

    Настроил кнопку, осталось сделать только кнопку на таймер

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,110 @@
-package sample;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane Form;
+
+    @FXML
+    private GridPane FormGridPane;
+
+    @FXML
+    private Label FormLabel1;
+
+    @FXML
+    private Label FormLabelInfo1;
+
+    @FXML
+    private Label FormLabel2;
+
+    @FXML
+    private Label FormLabelInfo2;
+
+    @FXML
+    private Label FormLabel3;
+
+    @FXML
+    private Label FormLabelInfo3;
+
+    @FXML
+    private CheckBox FormCeckBox1;
+
+    @FXML
+    private CheckBox FormCeckBox2;
+
+    @FXML
+    private CheckBox FormCeckBox3;
+
+    @FXML
+    private TextField FormTextEdit;
+
+    @FXML
+    private Button FormButtom;
+
+    @FXML
+    private TextField FormTextEditNotification;
+
+    @FXML
+    void initialize() {
+
+        falseTimer();
+
+        FormCeckBox1.setOnAction(event -> {
+            if (FormCeckBox1.isSelected()){
+              FormCeckBox2.setSelected(false);
+              FormCeckBox3.setSelected(false);
+            }
+        });
+
+        FormCeckBox2.setOnAction(event -> {
+            if (FormCeckBox2.isSelected()) {
+                FormCeckBox1.setSelected(false);
+                FormCeckBox3.setSelected(false);
+                }
+            });
+
+        FormCeckBox3.setOnAction(event -> {
+            if (FormCeckBox3.isSelected()) {
+                FormCeckBox1.setSelected(false);
+                FormCeckBox2.setSelected(false);
+            }
+        });
+
+        FormButtom.setOnAction(event -> {
+            if (FormCeckBox1.isSelected()){
+                FormCeckBox1.setSelected(false);
+                GoTimer(FormLabel1,FormLabelInfo1);
+            }else if (FormCeckBox2.isSelected()) {
+                FormCeckBox2.setSelected(false);
+                GoTimer(FormLabel2,FormLabelInfo2);
+            }else if (FormCeckBox3.isSelected()){
+                FormCeckBox3.setSelected(false);
+                GoTimer(FormLabel3,FormLabelInfo3);
+            }
+        });
+    }
+
+    public void GoTimer(Label labelTimer, Label labelInfo){
+        labelInfo.setText(FormTextEditNotification.getText());
+    }
+
+    public void falseTimer(){
+        FormLabel1.setText("00:00:00");
+        FormLabel2.setText("00:00:00");
+        FormLabel3.setText("00:00:00");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Timer @Kostanov");
+        primaryStage.setScene(new Scene(root, 600, 275));
         primaryStage.show();
     }
 
